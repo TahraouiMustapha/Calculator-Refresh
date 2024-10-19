f16ae0d957d65f25c355b881ed01f6410a5ee0f9
mustapha <moustaphaoussama1818@gmail.com>
2024-10-19 18:55:55 +0100

    feat : building functions for  the basic math operator

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
-console.log('fine')
+let firstNumber, 
+    secondNumber, 
+    operator = '';
+
+
+function operate(number1, operator, number2) {
+    number1 = Number(number1);
+    number2 = Number(number2);
+
+    switch (operator) {
+        case '+':
+            console.log(add(number1, number2));
+            break;
+        case '-':
+            console.log(subtract(number1, number2));
+            break;
+        case '*':
+            console.log(multiply(number1, number2));
+            break;    
+        case '/':
+            console.log(divide(number1, number2));
+            break; 
+        default :
+            console.log('no operator');                               
+    }
+}
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    if(b === 0) return "Error"
+    return a / b;
+}
